adde6d0b301a7d8e3d79e49f0863acb338ee51c4
Alikagn <alikagn@mail.ru>
2025-02-17 06:45:07 +0300

    На проверку

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,67 +1,66 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertPresenterDelegate {
+final class MovieQuizViewController: UIViewController,
+                                     QuestionFactoryDelegate,
+                                     AlertPresenterDelegate {
     // MARK: - Lifecycle
     
     //номер вопроса
     private var currentQuestionIndex = 0
-
+    
     //количество правильных ответов
     private var correctAnswers = 0
-
+    
     private let questionsAmount: Int = 10
     private var currentQuestion: QuizQuestion?
     private var questionFactory: QuestionFactoryProtocol?
     private var alertPresenter: AlertPresenterProtocol?
-    private let statisticService: StatisticServiceProtocol = StatisticService()
+    private var statisticService: StatisticServiceProtocol = StatisticService()
     
-    @IBOutlet private var textQueryLabel: UILabel!
-    @IBOutlet private var yesButton: UIButton!
-    @IBOutlet private var noButton: UIButton!
-    @IBOutlet private var textLabel: UILabel!
-    @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private weak var textQueryLabel: UILabel!
+    @IBOutlet private weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
-        
-        alertPresenter = AlertPresenter(delegate: self)
-        
         super.viewDidLoad()
-    
-        let questionFactory = QuestionFactory()
-        questionFactory.setup(delegate: self)
-        self.questionFactory = questionFactory
-        questionFactory.requestNextQuestion()
+        alertPresenter = AlertPresenter(delegate: self)
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
+        statisticService = StatisticService()
+        showLoadingIndicator()
+        questionFactory?.loadData()
     }
     
     // MARK: - QuestionFactoryDelegate
-
+    
     func didReceiveNextQuestion(question: QuizQuestion?) {
-        guard let question = question else {return}
+        guard let question else { return }
         currentQuestion = question
-            let viewModel = convert(model: question)
-            DispatchQueue.main.async { [weak self] in
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
             self?.showFirstScreen(quiz: viewModel)
-           }
         }
+    }
     
     func show(alert: UIAlertController) {
-           self.present(alert, animated: true)
-       }
-    
+        self.present(alert, animated: true)
+    }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-       disableButtons()
-        guard let currentQuestion = currentQuestion else {
+        changeStateButton(isEnabled: false)
+        guard let currentQuestion else {
             return
         }
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-        }
+    }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
-        disableButtons()
-        guard let currentQuestion = currentQuestion else {
+        changeStateButton(isEnabled: false)
+        guard let currentQuestion else {
             return
         }
         let givenAnswer = false
@@ -74,25 +73,24 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         
         if currentQuestionIndex == questionsAmount - 1 {
             statisticService.store(correct: correctAnswers, total: currentQuestionIndex + 1)
-                    let quizCount = statisticService.gamesCount
-                    let bestGame = statisticService.bestGame
-                    let formattedAccuracy = String(format: "%.2f", statisticService.totalAccuracy)
-                    let text = """
+            let quizCount = statisticService.gamesCount
+            let bestGame = statisticService.bestGame
+            let formattedAccuracy = String(format: "%.2f", statisticService.totalAccuracy)
+            let text = """
                     Ваш результат: \(correctAnswers)/\(questionsAmount)
                     Количество сыгранных квизов: \(quizCount)
                     Рекорд: \(bestGame.correct)/\(bestGame.total) (\(bestGame.date.dateTimeString))
                     Средняя точность: \(formattedAccuracy)%
                     """
-                    let results = QuizResultsViewModel(
-                        title: "Этот раунд окончен!",
-                        text: text,
-                        buttonText: "Сыграть еще раз")
-                    show(quiz: results)
-                    } else {
-                    currentQuestionIndex += 1
-                    questionFactory?.requestNextQuestion()
-                }
-        enableButtons()
+            let results = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть еще раз")
+            show(quiz: results)
+        } else {
+            currentQuestionIndex += 1
+            questionFactory?.requestNextQuestion()
+        }
     }
     
     private func showAnswerResult(isCorrect: Bool) {
@@ -102,25 +100,33 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
+            self.changeStateButton(isEnabled: true)
+
         }
     }
     
-     private func disableButtons() {
-         yesButton?.isEnabled = false
-         noButton?.isEnabled = false
-     }
-     
-     private func enableButtons() {
-         yesButton?.isEnabled = true
-         noButton?.isEnabled = true
-     }
-     
+    private func changeStateButton(isEnabled: Bool) {
+            noButton.isEnabled = isEnabled
+            yesButton.isEnabled = isEnabled
+    }
+   
+    /*
+    private func disableButtons() {
+        yesButton?.isEnabled = false
+        noButton?.isEnabled = false
+    }
+    
+    private func enableButtons() {
+        yesButton?.isEnabled = true
+        noButton?.isEnabled = true
+    }
+    */
+    
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let questionStep = QuizStepViewModel(
-            image: UIImage(named: model.image) ?? UIImage(),
-            question: model.text, // 3
+        return QuizStepViewModel(
+            image: UIImage(data: model.image) ?? UIImage(),
+            question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-        return questionStep
     }
     
     private func showFirstScreen(quiz step: QuizStepViewModel) {
@@ -130,19 +136,54 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     }
     
     private func show(quiz result: QuizResultsViewModel) {
-           let alertModel = AlertModel(
-               title: result.title,
-               message: result.text,
-               buttonText: result.buttonText,
-               buttonPress: { [weak self] in
-                   guard let self = self else { return }
-                   self.currentQuestionIndex = 0
-                   self.correctAnswers = 0
-                   self.questionFactory?.requestNextQuestion()
-               }
-           )
-           alertPresenter?.show(alertModel: alertModel)
+        let alertModel = AlertModel(
+            title: result.title,
+            message: result.text,
+            buttonText: result.buttonText,
+            buttonPress: { [weak self] in
+                guard let self = self else { return }
+                self.currentQuestionIndex = 0
+                self.correctAnswers = 0
+                self.questionFactory?.requestNextQuestion()
+            }
+        )
+        alertPresenter?.show(alertModel: alertModel)
+    }
+    
+    private func showLoadingIndicator() {
+        activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
+        activityIndicator.startAnimating() // включаем анимацию
+    }
+    
+    private func hideLoadingIndicator() {
+           activityIndicator.stopAnimating()
+           activityIndicator.hidesWhenStopped = true
        }
+    
+    private func showNetworkError(message: String) {
+        hideLoadingIndicator() // скрываем индикатор загрузки
+        
+        // создайте и покажите алерт
+        let model = AlertModel(title: "Ошибка",
+                               message: message,
+                               buttonText: "Попробовать еще раз") { [weak self] in
+            guard let self else { return }
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.questionFactory?.requestNextQuestion()
+        }
+        
+        alertPresenter?.show(alertModel: model)
+    }
+    
+    func didLoadDataFromServer() {
+        activityIndicator.isHidden = true // скрываем индикатор загрузки
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error) {
+        showNetworkError(message: error.localizedDescription) // возьмём в качестве сообщения описание ошибки
+    }
 }
 
 /*
@@ -207,16 +248,4 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-*/
-
-//Всего правильных ответов: \(statisticService.correctAnswers)
-
-/*
- // Получаем словарь всех значений
- let allValues = UserDefaults.standard.dictionaryRepresentation()
-
- // Получаем все ключи словаря, затем в цикле удаляем их
- allValues.keys.forEach { key in
-     UserDefaults.standard.removeObject(forKey: key)
- }
  */
